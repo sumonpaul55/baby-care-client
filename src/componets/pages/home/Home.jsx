fa64ee6f9b18ce334c55e72b63015d74e1330776
sumon paul <sumpaul100@gmail.com>
2023-11-05 12:48:05 +0600

    a section for our mission

--- src/componets/pages/home/Home.jsx
+++ src/componets/pages/home/Home.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Slider from './Slider';
 import { Helmet } from 'react-helmet-async';
-
+import ReactPlayer from 'react-player'
+import img from "../../../assets/thumbnail.jpg"
+import "./home.css"
 const Home = () => {
     return (
         <>
             <Helmet>
                 <title>Littlestarscare || home</title>
             </Helmet>
-            <main className='bg-slate-200'>
+            <main className='bg-slate-50'>
                 <Slider></Slider>
+                <section className='py-32 bg-white our-mission'>
+                    <div className="container mx-auto px-1">
+                        <div className='flex flex-col lg:flex-row gap-5 md:gap-16'>
+                            <div className='lg:w-5/12 clear-left space-y-8'>
+                                <h2 className='text-xl md:text-2xl lg:text-4xl font-semibold'>Our mission</h2>
+                                <p className='text-slate-600 md:text-xl tracking-tight'>Our mission at Littlestascare is to provide a nurturing haven where every childs potential is celebrated, and their natural curiosity is cultivated. Were committed to creating a safe, loving environment that fosters growth, learning, and positive development in our little stars. With a dedicated team and a passion for education, were here to ignite the spark in each child, guiding them on a path to a bright future filled with endless possibilities.</p>
+                                <p></p>
+                            </div>
+                            <div className='lg:w-7/12'>
+                                <ReactPlayer url='https://www.youtube.com/watch?v=PEWXRjgLdeM' width="100%" controls light={<img src={img} className='rounded-lg' alt='Thumbnail' />} />
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     );
--- src/componets/pages/home/Slider.jsx
+++ src/componets/pages/home/Slider.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import slider1 from "../../../assets/slider1.jpg"
+import slider2 from "../../../assets/slider2.jpg"
+import slider3 from "../../../assets/slider3.jpg"
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 const responsive = {
@@ -24,17 +26,30 @@ const responsive = {
 const Slider = () => {
     return (
         <Carousel responsive={responsive} infinite className=''>
+            <div className='bg-slate-200 relative flex rounded-lg'>
+                <div className='absolute bg-primary opacity-80 text-white lg:max-w-[500px] top-full lg:top-[90%] -translate-y-full left-[10%] p-2 md:p-10 rounded-lg space-y-5 w-full'>
+                    <h2 className="md:text-3xl font-bold">Where Little Stars Shine Bright</h2>
+                    <p className='text-sm md:text-lg hidden sm:block'>Where Little Stars Shine Bright - Nurturing young minds to radiate their potential in a loving, safe, and inspiring environment. Your childs path to brilliance starts here.</p>
+                    <button className='font-semibold text-sm md:text-xl bg-white text-primary sm:px-4 sm:py-2 rounded-full p-1'>Join the fall fun</button>
+                </div>
+                <img src={slider1} className='md:h-[550px] ml-auto rounded-r-lg' alt="" />
+            </div>
             <div className='bg-slate-200 relative flex'>
-                <div className='absolute bg-primary opacity-80 text-white lg:max-w-[500px] top-full lg:top-[90%] -translate-y-full left-[10%] p-2 md:p-10 rounded-lg space-y-5'>
+                <div className='absolute bg-primary opacity-80 text-white lg:max-w-[500px] top-full lg:top-[90%] -translate-y-full left-[10%] p-2 md:p-10 rounded-lg space-y-5 w-full'>
                     <h2 className="md:text-3xl font-bold">Where Little Stars Shine Bright</h2>
                     <p className='text-sm md:text-lg hidden sm:block'>Where Little Stars Shine Bright - Nurturing young minds to radiate their potential in a loving, safe, and inspiring environment. Your childs path to brilliance starts here.</p>
                     <button className='font-semibold text-sm md:text-xl bg-white text-primary sm:px-4 sm:py-2 rounded-full'>Join the fall fun</button>
                 </div>
-                <img src={slider1} className='md:h-[550px] ml-auto' alt="" />
+                <img src={slider2} className='md:h-[550px] ml-auto rounded-r-lg' alt="" />
             </div>
-            <div className='py-20'>Item 1</div>
-            <div className=''>Item 1</div>
-            <div className=''>Item 1</div>
+            <div className='bg-slate-200 relative flex'>
+                <div className='absolute bg-primary opacity-80 text-white lg:max-w-[500px] top-full lg:top-[90%] -translate-y-full left-[10%] p-2 md:p-10 rounded-lg space-y-5 w-full'>
+                    <h2 className="md:text-3xl font-bold">Where Little Stars Shine Bright</h2>
+                    <p className='text-sm md:text-lg hidden sm:block'>Where Little Stars Shine Bright - Nurturing young minds to radiate their potential in a loving, safe, and inspiring environment. Your childs path to brilliance starts here.</p>
+                    <button className='font-semibold text-sm md:text-xl bg-white text-primary sm:px-4 sm:py-2 rounded-full'>Join the fall fun</button>
+                </div>
+                <img src={slider3} className='md:h-[550px] ml-auto rounded-r-lg' alt="" />
+            </div>
         </Carousel>
     );
 };
