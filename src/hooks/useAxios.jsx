3120f29fcdb927ada5ca4665c84caf94d24f7dc7
sumon paul <sumpaul100@gmail.com>
2023-11-08 11:17:37 +0600

    chekcing live link

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,10 +1,9 @@
 import axios, { } from 'axios';
-// 
-// 
+
+
 // "http://localhost:5000"
-// import axios from 'axios';
 // const baseURL = "https://littlestarscare-server.vercel.app"
-const baseURL = "http://localhost:5000"
+const baseURL = "https://littlestarscare-server.vercel.app"
 const useAxiosSecure = axios.create({
     baseURL: baseURL,
     withCredentials: true,
