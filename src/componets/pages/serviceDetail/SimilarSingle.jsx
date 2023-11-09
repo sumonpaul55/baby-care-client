5e018517e3e94946e50b34c9fe6f159e0a28e086
sumon paul <sumpaul100@gmail.com>
2023-11-09 13:30:02 +0600

    related service name updated

--- src/componets/pages/serviceDetail/SimilarSingle.jsx
+++ src/componets/pages/serviceDetail/SimilarSingle.jsx
@@ -10,7 +10,7 @@ const SimilarSingle = ({ similarData }) => {
     const { serviceName, serviceImg, price, category, _id } = similarData;
     return (
         <div className='p-3 border bg-slate-300 shadow-lg md:p-5'>
-            <h2 className='font-bold mb-3 md:text-xl md:mb-4'>{serviceName}</h2>
+            <h2 className='font-bold mb-3 md:text-base md:mb-4'>{serviceName}</h2>
             <img src={serviceImg} className='w-full' alt="" />
             {/* <Link to={`/service-detail/${_id}`} className='bg-pink-600 py-1 px-2 text-white block mt-5 md:py-3 text-center'>View </Link> */}
             <div className='space-y-2'>
